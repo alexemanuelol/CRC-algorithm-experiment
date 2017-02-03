852f65c6ca317bb59a2cb649c417e31d42eb08f3
David Isaksson <davve___93@hotmail.com>
2017-02-03 12:55:36 +0100

    Updated test algorithm

--- main.m
+++ main.m
@@ -1,43 +1,48 @@
 clc;
 clf;
 
-
-error           = 0;    % Errors so far
-insertedErrors  = 1;    % Bits to flip
+insertedErrors  = 10;    % Bits to flip
+error           = zeros(1,insertedErrors + 1);    % Errors so far
 cycles          = 100;  % cycles to iterate
-
+dataLength      = 2000;
 % Create data to send
-data = round(rand(1,5000));
+data = round(rand(1,dataLength));
 % data = [0 1 1 0 1 1 0 1];
 
 % Select polynomial 
+poly = [1 0 1 0 1 0 1];
 % poly = [1 0 1 1]; 
-% poly = hexToBinaryVector('0x19') % 5 bit
-% poly = hexToBinaryVector('0x09') % 7 bit
-% poly = hexToBinaryVector('0x1D') % 8 bit
-% poly = hexToBinaryVector('0x1021') % 16 bit
+% poly = hexToBinaryVector('0x19'); % 5 bit
+% poly = hexToBinaryVector('0x09'); % 7 bit
+% poly = hexToBinaryVector('0x1D'); % 8 bit
+% poly = hexToBinaryVector('0x1021'); % 16 bit
 % poly = hexToBinaryVector('0x04C11DB7'); % 32 bit
 % poly = [1 0 1 1 1 0 1 0 0 0 0 0 1 1 0 1 1 1 0 0 0 1 1 0 0 1 1 0 1 0 1 1];
-poly = hexToBinaryVector('0x42F0E1EBA9EA369'); % 64 bit
+% poly = hexToBinaryVector('0x42F0E1EBA9EA369'); % 64 bit
 
-for i=1:cycles
-    % Create codeword to send through channel
-    codeword = addCRC(data, poly);
 
-    % Sending through channel...
-    codeword = flipBits(codeword, insertedErrors);
+for j=1:insertedErrors + 1
+    for i=1:cycles
+        % Create codeword to send through channel
+        codeword = addCRC(data, poly);
 
-    % Check if error occured
-    recieved = calcReminder(codeword, poly);
-    
-    % Checks if any ones in vector 
-    error = error + max(recieved); 
+        % Sending through "noisy" channel...
+        codeword = flipBits(codeword, j - 1);
+
+        % Check if error occured
+        recieved = calcReminder(codeword, poly);
+
+        % Checks if any ones in vector 
+        error(j) = error(j) + max(recieved); 
+    end
 end
-
 
 % Prints the number of cycles, inserted errors and found errors
 cycles 
 polyLength = length(poly)
-dataLength = length(data)
+dataLength
 insertedErrors
-error
+error'
+
+plot(error)
+axis([0 insertedErrors 0 (cycles+ cycles/10)])

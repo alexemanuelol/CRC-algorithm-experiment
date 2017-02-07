2ba64f5db0705996fa858f51fa970fbf07258929
David Isaksson <davve___93@hotmail.com>
2017-02-07 09:18:09 +0100

    Added stats

--- main.m
+++ main.m
@@ -1,20 +1,28 @@
 clc;
 clf;
 
-insertedErrors  = 10;    % Bits to flip
-error           = zeros(1,insertedErrors + 1);    % Errors so far
-cycles          = 100;  % cycles to iterate
-dataLength      = 2000;
+format LONG
+
+insertedErrors  = 10;     % Bits to flip
+error           = zeros(1,insertedErrors + 1);    % Num of recognized faulty data strings
+errorPercent    = zeros(1,insertedErrors + 1);    % Errors so far
+cycles          = 1000;   % cycles to iterate
+dataLength      = 2000;   % Bits to send
+
 % Create data to send
 data = round(rand(1,dataLength));
 % data = [0 1 1 0 1 1 0 1];
 
 % Select polynomial 
-poly = [1 0 1 0 1 0 1];
+% poly = [1 0 1 0 1 0 1];
 % poly = [1 0 1 1]; 
 % poly = hexToBinaryVector('0x19'); % 5 bit
 % poly = hexToBinaryVector('0x09'); % 7 bit
+% poly = [1 0 0 0 1 1 1 0 1]; 
+poly = [1 1 1 0 1 0 1 0 1];
 % poly = hexToBinaryVector('0x1D'); % 8 bit
+% poly = [1 0 0 0 1 0 0 0 0 0 0 1 0 0 0 0 1];
+% poly = [1 1 0 0 0 0 0 0 0 0 0 0 0 0 1 0 1];
 % poly = hexToBinaryVector('0x1021'); % 16 bit
 % poly = hexToBinaryVector('0x04C11DB7'); % 32 bit
 % poly = [1 0 1 1 1 0 1 0 0 0 0 0 1 1 0 1 1 1 0 0 0 1 1 0 0 1 1 0 1 0 1 1];
@@ -35,6 +43,7 @@ for j=1:insertedErrors + 1
         % Checks if any ones in vector 
         error(j) = error(j) + max(recieved); 
     end
+    j
 end
 
 % Prints the number of cycles, inserted errors and found errors
@@ -44,5 +53,14 @@ dataLength
 insertedErrors
 error'
 
-plot(0:length(error) - 1, error)
-axis([0 (insertedErrors) 0 (cycles+ cycles/10)])
+% Calculates the percentage
+for i=2:insertedErrors + 1
+    errorPercent(i) = error(i)/cycles;
+end
+errorPercent'
+errorMean = mean(errorPercent(2:insertedErrors))
+polyRatio = 1 - 1 / (2^(polyLength - 1)) % How many percent that the polynome should be able to handle 
+
+% Diagram
+bar(0:length(error) - 1, error)
+axis([-0.5 (insertedErrors+1) 0 (cycles+cycles/10)])
